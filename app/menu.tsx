5180910d995601f72460a3947de4480db143d0de
Nicolas Baque <nicoware@hotmail.fr>
2024-11-27 20:27:23 +0100

    responsive and posters

--- app/menu.tsx
+++ app/menu.tsx
@@ -9,9 +9,9 @@ export default function Menu() {
   const { data, error, isLoading } = useMovieQuery(debounceQuery);
 
   return (
-    <div className="flex ">
+    <div className="flex h-svh items-center gap-16 md:flex-row flex-col ">
       {" "}
-      <form>
+      <form className="flex flex-col justify-center items-center md:items-start md:mt-0 mt-20">
         <Title>Wanna search for a movie?</Title>
         <input
           type="text"
@@ -23,13 +23,18 @@ export default function Menu() {
           }}
         />
       </form>
-      <div className="grow">
+      <div className="grow overflow-scroll h-4/5 no-scrollbar grid md:grid-cols-3 sm:grid-cols-2 :grid-cols-1 gap-6 md:mb-0 mb-20">
         {error ? <p>Error : {error.message}</p> : null}
         {isLoading ? <p>Loading ...</p> : null}
         {data?.Search?.length > 0
-          ? data?.Search.map((movie: { imdbID: string; Title: string }) => (
-              <p key={movie.imdbID}>{movie.Title}</p>
-            ))
+          ? data?.Search.map(
+              (movie: { imdbID: string; Title: string; Poster: string }) => (
+                <div key={movie.imdbID}>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <p>{movie.Title}</p>
+                </div>
+              )
+            )
           : null}
       </div>
     </div>
