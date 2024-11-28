40e12fcf5bd3d046664ef41b782ef9ebdad323c0
Nicolas Baque <nbaque@questel.com>
2024-11-28 17:48:02 +0100

    rename title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import { NuqsAdapter } from "nuqs/adapters/next/app";
 import "./globals.css";
 
+export const metadata = {
+  title: "Movie Search App",
+  description: "Let's search for a movie!",
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
